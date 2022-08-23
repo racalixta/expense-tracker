f8d3459cb873c3853667ad1e0debaca89170fee7
racalixta <ra.calixta@gmail.com>
2022-08-22 21:44:03 -0300

    atualizacoes e build

---
+++ src/App.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+    background-color: #111111;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    padding-top: .9em;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 6em;
+`;
+
+
+
+
+
---
+++ src/components/TableItem/index.tsx
@@ -0,0 +1,30 @@
+import { Item } from '../../types/Item';
+import * as C from './styles';
+import { formatDate } from '../../helpers/dateFilter';
+import { categories } from '../../data/categories';
+
+type Props = {
+    item: Item
+}
+
+export const TableItem = ({ item }: Props) => {
+    return (
+        <C.TableLine>
+            <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
+            <C.TableColumn>
+                <C.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </C.Category>
+            </C.TableColumn>
+            <C.TableColumn>{item.title}</C.TableColumn>
+            <C.TableColumn>
+                <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+                    R$ {item.value}
+                </C.Value>
+            </C.TableColumn>
+            {/* 1:05:00     APARENTA ESTAR FUNCIONANDO MESMO COM O ERRO ACIMA */}
+            {/* 1:11:54 aparece erro de item.category nao pode ser usado como index */}
+        </C.TableLine>
+    );
+}
+
